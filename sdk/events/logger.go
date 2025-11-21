7698ab1aae4367296a1256d9d54c38f72e36ca08
Gwen Le Viavant <gwen.le-viavant@glimps.re>
2025-11-21 14:37:07 +0100

    feat: console logger handle attrs and groups

--- sdk/events/logger.go
+++ sdk/events/logger.go
@@ -3,6 +3,7 @@ package events
 import (
 	"context"
 	"log/slog"
+	"slices"
 	"strings"
 	"time"
 )
@@ -12,49 +13,107 @@ type EventLogHandler interface {
 }
 
 type LogEvent struct {
-	Level   string `json:"level" validate:"required,oneof=error warn info debug"`
-	Message string `json:"message" validate:"required"`
-	Time    int64  `json:"time" validate:"required"`
+	Level      string         `json:"level" validate:"required,oneof=error warn info debug"`
+	Message    string         `json:"message" validate:"required"`
+	Time       int64          `json:"time" validate:"required"`
+	Attributes map[string]any `json:"attributes,omitempty"`
+}
+
+type attrWithGroups struct {
+	attr   slog.Attr
+	groups []string
 }
 
 type LogHandler struct {
 	eventPusher Notifier
 	leveler     slog.Leveler
+	attributes  []attrWithGroups
+	groups      []string
 }
 
 func (h *Handler) GetLogHandler() slog.Handler {
 	return h.logHandler
 }
 
-func (c LogHandler) Enabled(ctx context.Context, level slog.Level) bool {
+func (lh LogHandler) Enabled(ctx context.Context, level slog.Level) bool {
 	minLevel := slog.LevelInfo
-	if c.leveler != nil {
-		minLevel = c.leveler.Level()
+	if lh.leveler != nil {
+		minLevel = lh.leveler.Level()
 	}
 	return level >= minLevel
 }
 
-func (c LogHandler) Handle(ctx context.Context, record slog.Record) (err error) {
+func (lh LogHandler) Handle(ctx context.Context, record slog.Record) (err error) {
 	log := LogEvent{
-		Time:    record.Time.Unix(),
-		Message: record.Message,
-		Level:   strings.ToLower(record.Level.String()),
+		Time:       record.Time.Unix(),
+		Message:    record.Message,
+		Level:      strings.ToLower(record.Level.String()),
+		Attributes: lh.getAttributes(record),
 	}
 
 	ctx, cancel := context.WithTimeout(ctx, time.Second*15)
 	defer cancel()
 
-	err = c.eventPusher.Notify(ctx, log)
+	err = lh.eventPusher.Notify(ctx, log)
 	if err != nil {
 		return
 	}
 	return
 }
 
-func (c LogHandler) WithAttrs(attrs []slog.Attr) (newHandler slog.Handler) {
-	return c
+func (lh LogHandler) WithAttrs(attrs []slog.Attr) (newHandler slog.Handler) {
+	if len(attrs) == 0 {
+		return lh
+	}
+	lh.attributes = slices.Clip(lh.attributes) // to force append() to allocate a new backing array, to prevent sub handlers from overwriting each other's data
+	groups := slices.Clip(lh.groups)
+	for _, attr := range attrs {
+		lh.attributes = append(lh.attributes, attrWithGroups{
+			attr:   attr,
+			groups: groups,
+		})
+	}
+	return lh
 }
 
-func (c LogHandler) WithGroup(name string) (newHandler slog.Handler) {
-	return c
+func (lh LogHandler) WithGroup(name string) (newHandler slog.Handler) {
+	if name == "" {
+		return lh
+	}
+	lh.groups = slices.Clip(lh.groups)
+	lh.groups = append(lh.groups, name)
+	return lh
+}
+
+func (lh LogHandler) getAttributes(record slog.Record) (attributes map[string]any) {
+	attributes = make(map[string]any)
+	for _, a := range lh.attributes {
+		addAttribute(attributes, a.groups, a.attr)
+	}
+	record.Attrs(func(attr slog.Attr) bool {
+		addAttribute(attributes, lh.groups, attr)
+		return true
+	})
+	return
+}
+
+func addAttribute(attributes map[string]any, groups []string, attr slog.Attr) {
+	if len(groups) == 0 {
+		attributes[attr.Key] = attr.Value.Any()
+		return
+	}
+
+	current := attributes
+	for _, group := range groups {
+		if rawGroupMap, exists := current[group]; exists {
+			if groupMap, ok := rawGroupMap.(map[string]any); ok {
+				current = groupMap
+				continue
+			}
+		}
+		groupMap := make(map[string]any)
+		current[group] = groupMap
+		current = groupMap
+	}
+	current[attr.Key] = attr.Value.Any()
 }
--- sdk/events/logger_test.go
+++ sdk/events/logger_test.go
@@ -4,38 +4,204 @@ import (
 	"context"
 	"log/slog"
 	"testing"
+
+	"github.com/google/go-cmp/cmp"
+	"github.com/google/go-cmp/cmp/cmpopts"
 )
 
 func TestLogHandler(t *testing.T) {
-	type args struct {
-		notifier Notifier
-		msg      string
-	}
 	tests := []struct {
-		name string
-		args args
+		name             string
+		log              func(logger *slog.Logger)
+		logLevel         slog.Level
+		wantEvent        LogEvent
+		wantNotifyCalled bool
 	}{
 		{
 			name: "ok",
-			args: args{
-				notifier: notifierMock{
-					notifyMock: func(ctx context.Context, event any) (err error) {
-						_, ok := event.(LogEvent)
-						if !ok {
-							t.Errorf("event is not log event")
-						}
-						return
+			log: func(logger *slog.Logger) {
+				l1 := logger.With(slog.String("attr1", "toto"), slog.Int("attr2", 1))
+				subLogger := l1.WithGroup("group1")
+				subLogger.Info("toto", slog.String("test", "nested toto"))
+			},
+			wantEvent: LogEvent{
+				Level:   "info",
+				Message: "toto",
+				Attributes: map[string]any{
+					"attr1": "toto",
+					"attr2": int64(1),
+					"group1": map[string]any{
+						"test": "nested toto",
 					},
 				},
-				msg: "message",
+			},
+			wantNotifyCalled: true,
+		},
+		{
+			name:     "ok debug",
+			logLevel: slog.LevelDebug,
+			log: func(logger *slog.Logger) {
+				l1 := logger.With(slog.String("attr1", "toto"), slog.Int("attr2", 1))
+				subLogger := l1.WithGroup("group1")
+				l2 := subLogger.With(slog.Any("attr3", []string{"test1", "test2"}))
+				l2.WithGroup("group2").WithGroup("group3").Debug("toto debug", slog.String("attr4", "test4"), "test", "toto") //nolint:sloglint // made on purpose to check that it's ok
+			},
+			wantEvent: LogEvent{
+				Level:   "debug",
+				Message: "toto debug",
+				Attributes: map[string]any{
+					"attr1": "toto",
+					"attr2": int64(1),
+					"group1": map[string]any{
+						"attr3": []string{"test1", "test2"},
+						"group2": map[string]any{
+							"group3": map[string]any{
+								"attr4": "test4",
+								"test":  "toto",
+							},
+						},
+					},
+				},
+			},
+			wantNotifyCalled: true,
+		},
+		{
+			name:     "ok duplicate arg key",
+			logLevel: slog.LevelDebug,
+			log: func(logger *slog.Logger) {
+				subLogger := logger.With(slog.String("arg1", "test1"))
+				subLogger.Debug("toto debug", slog.String("arg1", "test2"))
+			},
+			wantEvent: LogEvent{
+				Level:   "debug",
+				Message: "toto debug",
+				Attributes: map[string]any{
+					"arg1": "test2",
+				},
+			},
+			wantNotifyCalled: true,
+		},
+		{
+			name:     "ok duplicate arg/group key",
+			logLevel: slog.LevelDebug,
+			log: func(logger *slog.Logger) {
+				subLogger := logger.With(slog.String("group1", "test1"))
+				subLogger.WithGroup("group1").Debug("toto debug", slog.String("arg1", "test1"))
+			},
+			wantEvent: LogEvent{
+				Level:   "debug",
+				Message: "toto debug",
+				Attributes: map[string]any{
+					"group1": map[string]any{
+						"arg1": "test1",
+					},
+				},
+			},
+			wantNotifyCalled: true,
+		},
+		{
+			name:     "ok sub loggers from same parent", // to check their own attributes are preserved
+			logLevel: slog.LevelDebug,
+			log: func(logger *slog.Logger) {
+				originalHandler := logger.Handler().(*LogHandler)
+				attrs := make([]attrWithGroups, 1, 2) // force spare capacity to guarantee append() will write into same backing array
+				attrs[0] = attrWithGroups{attr: slog.String("common", "val")}
+				handler := LogHandler{
+					eventPusher: originalHandler.eventPusher,
+					leveler:     originalHandler.leveler,
+					attributes:  attrs,
+				}
+				parent := slog.New(handler)
+				sub1 := parent.With(slog.String("sub", "1"))
+				_ = parent.With(slog.String("sub", "2")) // to check this operation does not overwrite sub1's attribute in the shared backing array
+				sub1.Debug("msg")
+			},
+			wantEvent: LogEvent{
+				Level:   "debug",
+				Message: "msg",
+				Attributes: map[string]any{
+					"common": "val",
+					"sub":    "1",
+				},
+			},
+			wantNotifyCalled: true,
+		},
+		{
+			name:     "ok sub loggers from same parent with groups", // to check their own groups are preserved
+			logLevel: slog.LevelDebug,
+			log: func(logger *slog.Logger) {
+				originalHandler := logger.Handler().(*LogHandler)
+				groups := make([]string, 1, 2) // force spare capacity to guarantee append() will write into same backing array
+				groups[0] = "g1"
+				handler := LogHandler{
+					eventPusher: originalHandler.eventPusher,
+					leveler:     originalHandler.leveler,
+					groups:      groups,
+				}
+				parent := slog.New(handler)
+				sub1 := parent.WithGroup("sub1")
+				_ = parent.WithGroup("sub2") // to check this operation does not overwrite sub1's group in the shared backing array
+				sub1.Debug("msg", slog.String("key", "val"))
+			},
+			wantEvent: LogEvent{
+				Level:   "debug",
+				Message: "msg",
+				Attributes: map[string]any{
+					"g1": map[string]any{
+						"sub1": map[string]any{
+							"key": "val",
+						},
+					},
+				},
+			},
+			wantNotifyCalled: true,
+		},
+		{
+			name: "ok no attributes",
+			log: func(logger *slog.Logger) {
+				logger.Info("hello")
+			},
+			wantEvent: LogEvent{
+				Level:      "info",
+				Message:    "hello",
+				Attributes: map[string]any{},
+			},
+			wantNotifyCalled: true,
+		},
+		{
+			name:     "ok debug not called",
+			logLevel: slog.LevelInfo,
+			log: func(logger *slog.Logger) {
+				logger.Debug("toto debug", slog.String("arg1", "test1"))
 			},
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			h := NewHandler(tt.args.notifier, &slog.LevelVar{}, map[ErrorEventType]string{})
+			notifyCalled := false
+			notifier := notifierMock{
+				notifyMock: func(ctx context.Context, event any) (err error) {
+					ev, ok := event.(LogEvent)
+					if !ok {
+						t.Errorf("event is not log event")
+					}
+					if diff := cmp.Diff(ev, tt.wantEvent, cmpopts.IgnoreFields(LogEvent{}, "Time")); diff != "" {
+						t.Fatalf("Notifier diff in received log, diff(got-want)=%s", diff)
+					}
+					notifyCalled = true
+					return
+				},
+			}
+			leveler := &slog.LevelVar{}
+			leveler.Set(tt.logLevel)
+
+			h := NewHandler(notifier, leveler, map[ErrorEventType]string{})
 			l := slog.New(h.GetLogHandler())
-			l.Info(tt.args.msg)
+			tt.log(l)
+
+			if notifyCalled != tt.wantNotifyCalled {
+				t.Errorf("notify called: %v, want %v", notifyCalled, tt.wantNotifyCalled)
+			}
 		})
 	}
 }
